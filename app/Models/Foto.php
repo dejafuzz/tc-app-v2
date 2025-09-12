ff3c35f905340c5b22aa2009709f0e68f0f233ce
Ikmal <fajrikmal141@gmail.com>
2025-09-12 16:10:38 +0700

    add resetAntrian function

--- app/Models/Foto.php
+++ app/Models/Foto.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Foto extends Model
 {
@@ -19,5 +20,28 @@ class Foto extends Model
     public function pesanan()
     {
         return $this->belongsTo(Pesanan::class,'pesanan_id','id_pesanan');
+    }
+
+    public static function resetAntrian()
+    {
+        DB::transaction(function () {
+            DB::table('foto')->update(['antrian' => null]);
+
+            DB::statement("
+                UPDATE foto f
+                JOIN (
+                    SELECT id_foto, ROW_NUMBER() OVER (
+                        ORDER BY
+                            CASE WHEN status_foto = 'Editing' THEN 1
+                                WHEN status_foto = 'List File Edit' THEN 2
+                                ELSE 3 END,
+                            tanggal_list ASC
+                    ) AS rn
+                    FROM foto
+                    WHERE status_foto IN ('Editing', 'List File Edit')
+                ) o ON f.id_foto = o.id_foto
+                SET f.antrian = o.rn
+            ");
+        });
     }
 }
