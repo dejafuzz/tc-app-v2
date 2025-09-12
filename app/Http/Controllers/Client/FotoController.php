5812174c0d6f95dfe7951fab7005ad1849ca5b4e
Ikmal <fajrikmal141@gmail.com>
2025-09-12 16:11:35 +0700

    add and fix antrian editing

--- app/Http/Controllers/Client/FotoController.php
+++ app/Http/Controllers/Client/FotoController.php
@@ -6,22 +6,24 @@ use App\Http\Controllers\Controller;
 use App\Models\Foto;
 use App\Models\Pesanan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class FotoController extends Controller
 {
     public function add_list_foto(Request $request,$id)
     {
-        $antrian = Foto::orderBy('antrian', 'desc')->first()->antrian;
-        $antrianFoto = Foto::where('status_foto', 'Editing')->orderBy('antrian','desc')->first();
+        $antrianTerakhir = Foto::max('antrian') ?? 0;
         
-
         $pesananId = Pesanan::where('booking_id',$id)->first()->id_pesanan;
         $foto = Foto::where('pesanan_id',$pesananId)->first();
         $foto_edit = json_encode($request->foto_edit);
         $foto->foto_edit = $foto_edit;
         $foto->status_foto = 'List File Edit';
-        // $foto->antrian = $antrian + 1;
+        $foto->antrian = $antrianTerakhir + 1;
+        $foto->tanggal_list = Carbon::now();
         $foto->save();
+
+        Foto::resetAntrian();
 
         return redirect()->back()->with('success','Permintaan berhasil dikirim');
 
